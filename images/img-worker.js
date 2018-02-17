ef739f092c44198a07856e91c8aaa8f6b2893f70
Андрей <levi2ki1988@gmail.com>
2018-02-17 11:53:50 +0400

    add error handler  Changes to be committed: 	modified:   images/img-worker.js

--- images/img-worker.js
+++ images/img-worker.js
@@ -2,6 +2,8 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll(['/test-pwa/images/', '/test-pwa/images/1.jpg'])
+    }).catch(err => {
+      console.warn('error caching', err)
     })
   )
 });
