1cbd5eefe56867ca47b6951ac50ac88691d5807b
Андрей <levi2ki1988@gmail.com>
2018-02-17 11:56:10 +0400

    modified:   images/img-worker.js

--- images/img-worker.js
+++ images/img-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(cache => {
-      return cache.addAll(['/test-pwa/images/', '/test-pwa/images/1.jpg'])
+      return cache.addAll(['/test-pwa/images/1.jpg'])
     }).catch(err => {
       console.warn('error caching', err)
     })
